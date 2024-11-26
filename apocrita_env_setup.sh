be42e167ee33df4574331ea1bd02aa340a771ca1
Hannah <53306752+hlnicholls@users.noreply.github.com>
2024-11-26 12:40:52 +0000

    Create apocrita_env_setup.sh

---
+++ apocrita_env_setup.sh
@@ -0,0 +1,27 @@
+# Start a session on QMUL computing cluster with a maximum runtime of 8 hours and 20 GB of memory allocated.
+# If it fails to start set time to 1 hour (1:00:00)
+qlogin -l h_rt=8:00:00 -l h_vmem=20G 
+
+# Load the Anaconda module, which is used for managing Python environments and packages.
+module load anaconda3 
+
+# Create a new Python environment named "mantisml2_env" with Python version 3.8.
+conda create -n mantisml2_env python=3.8 
+
+# Add "conda-forge," a popular community-driven repository, to the list of sources for installing Python packages.
+conda config --append channels conda-forge 
+
+# Activate the newly created Python environment. This environment isolates the packages used in this project from others on your system.
+conda activate mantisml2_env 
+
+# Download the Mantis-ML software (version 2.0) from a public GitHub repository to your current directory.
+git clone https://github.com/astrazeneca-cgr-publications/mantis-ml-release-2.0.git 
+
+# Move into the directory containing the downloaded Mantis-ML files. (using 'cd' command to change directory)
+cd ./mantis-ml-release-2.0 
+
+# Install a specific version of the "setuptools" package, which is needed for managing Python package installations. 
+pip install setuptools==65.6.2 
+
+# Install the Mantis-ML software in "editable" mode, allowing you to make changes to the code that are immediately applied without reinstallation.
+pip install -e . 
