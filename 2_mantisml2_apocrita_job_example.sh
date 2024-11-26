17922386c98b6fde46c53a8167785372ac449230
Hannah <53306752+hlnicholls@users.noreply.github.com>
2024-11-26 13:16:28 +0000

    Create 2_mantisml2_apocrita_job_example.sh

---
+++ 2_mantisml2_apocrita_job_example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#$ -cwd # Set the working directory for the job to the current directory 
+#$ -pe smp 5 # Request 1 core 
+#$ -l h_rt=72:0:0 # Request 1 hour runtime 
+#$ -l h_vmem=10G # Request 1GB RAM 
+#$ -m bea 
+
+module load anaconda3 
+
+conda activate mantis-ml-2 
+
+cd ./mantis-ml-release-2 
+
+mantisml2 -d "hypertrophic cardiomyopathy" -o mantisml2_hcm_results 
