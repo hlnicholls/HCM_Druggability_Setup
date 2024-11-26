a8657eddd94fa1ef8ab309c9213566937f22d404
Hannah <53306752+hlnicholls@users.noreply.github.com>
2024-11-26 13:18:25 +0000

    Create 3_submitting_apocrita_job.sh

---
+++ 3_submitting_apocrita_job.sh
@@ -0,0 +1,9 @@
+# In the terminal, once connected to apocrita, run:
+
+qsub 2_mantisml2_apocrita_job_example.sh
+
+# The job is now submitted and will run in the background, even when you leave Apocrita
+
+# To check the status of your job run:
+
+qstat
